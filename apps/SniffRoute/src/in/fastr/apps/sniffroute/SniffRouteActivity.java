5ea87e26b9a5e5c91c13bdc258f6c272242d6b83
Gaurav Lochan <gaurav@fastr.in>
2011-09-14 18:09:14 +0530

    Adding SniffRoute empty android app

---
+++ apps/SniffRoute/src/in/fastr/apps/sniffroute/SniffRouteActivity.java
@@ -0,0 +1,13 @@
+package in.fastr.apps.sniffroute;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SniffRouteActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
