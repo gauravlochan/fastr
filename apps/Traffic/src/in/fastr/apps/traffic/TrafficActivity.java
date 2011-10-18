5c933c0fb56211b6809900df7a66629a4abc956f
Gaurav Lochan <gaurav@fastr.in>
2011-10-18 13:31:43 +0530

    Empty android app project "Traffic"

---
+++ apps/Traffic/src/in/fastr/apps/traffic/TrafficActivity.java
@@ -0,0 +1,13 @@
+package in.fastr.apps.traffic;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TrafficActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
