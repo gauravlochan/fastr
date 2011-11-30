91699909ffb7d598ef144c8b6f54e4ddcc72dd9b
Gaurav Lochan <gaurav@fastr.in>
2011-12-01 00:36:52 +0530

    Added common library.  This has functionality that can be used across apps

---
+++ apps/commonlib/src/in/fastr/library/Global.java
@@ -0,0 +1,11 @@
+package in.fastr.library;
+
+/**
+ * This class has a list of global constants that are used across the app
+ * 
+ * @author gaurav
+ */
+public class Global {
+    public static String Company = "fastr";
+
+}
---
+++ apps/commonlib/src/in/fastr/library/rest/RESTHelper.java
@@ -0,0 +1,136 @@
+package in.fastr.library.rest;
+
+import in.fastr.library.Global;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+
+import android.util.Log;
+
+public class RESTHelper {
+	
+
+    /**
+     * A simple method to make a REST call to the specified server.  Good for 
+     * testing
+     * 
+     * @param server
+     */
+    public static void simplePost(String server, JSONArray payload) { 
+        Log.d(Global.Company, "Attempting call to REST");
+
+        HttpClient httpClient = new DefaultHttpClient();
+        HttpPost httpPost = new HttpPost(server);
+        
+        HttpEntity requestEntity = null;
+        try {
+            requestEntity = new StringEntity(payload.toString());
+        } catch (UnsupportedEncodingException e1) {
+            // TODO Auto-generated catch block
+            e1.printStackTrace();
+        }
+        httpPost.setEntity(requestEntity);
+
+        try {
+            HttpResponse response = null;
+            response = httpClient.execute(httpPost);
+            
+            if (response != null) {
+                Log.d(Global.Company, "Successful call to REST");
+                Log.d(Global.Company, response.getStatusLine().toString());
+                
+                HttpEntity entity = response.getEntity();
+                if (entity != null) {
+                    InputStream instream = entity.getContent();
+                    String result = convertStreamToString(instream);
+                    Log.i(Global.Company, "Result of converstion: [" + result + "]");
+                    instream.close();
+                } else {
+                    Log.d(Global.Company, "Empty Http response");
+                }
+            } else {
+                Log.d(Global.Company, "Unsuccessful call to REST");
+            }
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+    }
+    
+    
+    /**
+     * A simple get call to the specified server.  Good for testing the 
+     * server connectivity
+     * 
+     * @param server
+     */
+    public static String simpleGet(String server) {
+        String result = null;
+        Log.d(Global.Company, "Attempting call to REST");
+
+        HttpClient httpClient = new DefaultHttpClient();
+        HttpGet httpGet = new HttpGet(server);
+        try {
+            HttpResponse response = null;
+            response = httpClient.execute(httpGet);
+            if (response != null) {
+                Log.d(Global.Company, "Successful call to REST");
+                Log.d(Global.Company, response.getStatusLine().toString());
+                
+                HttpEntity entity = response.getEntity();
+                if (entity != null) {
+                    InputStream instream = entity.getContent();
+                    result = convertStreamToString(instream);
+                    Log.i(Global.Company, "Result of converstion: [" + result + "]");
+                    instream.close();
+                } else {
+                    Log.d(Global.Company, "Empty Http response");
+                }
+            } else {
+                Log.d(Global.Company, "Unsuccessful call to REST");
+            }
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+    
+
+    private static String convertStreamToString(InputStream is) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        StringBuilder sb = new StringBuilder();
+        String line = null;
+        try {
+            while ((line = reader.readLine()) != null) {
+                sb.append(line + "\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                is.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return sb.toString();
+    }
+ 
+}
