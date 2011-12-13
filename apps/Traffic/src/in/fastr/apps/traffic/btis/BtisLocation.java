ecbd5ddb649f325804813a66507666c16e58e3fc
Gaurav Lochan <gaurav@fastr.in>
2011-12-13 18:43:54 +0530

    Added Btis json objects

---
+++ apps/Traffic/src/in/fastr/apps/traffic/btis/BtisLocation.java
@@ -0,0 +1,6 @@
+package in.fastr.apps.traffic.btis;
+
+// {"label":"White Field RW Station","lat":"12.99602778","lon":"77.76161111","status":"Slow","smscode":"WFRL"}]
+public class BtisLocation {
+
+}
---
+++ apps/Traffic/src/in/fastr/apps/traffic/btis/BtisResult.java
@@ -0,0 +1,9 @@
+package in.fastr.apps.traffic.btis;
+
+
+import java.util.List;
+
+public class BtisResult {
+	public List<BtisLocation> locations;
+	public String time;
+}
