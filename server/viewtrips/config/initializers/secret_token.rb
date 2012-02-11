13d003cc166ce03857792c5896d2a47887210514
Gaurav Lochan <gaurav@fastr.in>
2012-02-11 13:16:13 +0530

    Added rails project to view trips

---
+++ server/viewtrips/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Viewtrips::Application.config.secret_token = '71891af6883319a49c3eac7af5a41143afafa81b094515e07ec597b18441147b03a0cc9ba355aa4fbe39cd5d850cb9bf1d462f95f847ac053518d3a1e39b4d24'
