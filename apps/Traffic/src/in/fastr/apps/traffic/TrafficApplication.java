8e072aacc6c67176b535fb4e6ffc2a4ea993d9bc
Gaurav Lochan <gaurav@fastr.in>
2012-02-20 18:00:41 +0530

    Add support for ACRA

--- apps/Traffic/src/in/fastr/apps/traffic/TrafficApplication.java
+++ apps/Traffic/src/in/fastr/apps/traffic/TrafficApplication.java
@@ -1,13 +1,23 @@
 package in.fastr.apps.traffic;
 
+import greendroid.app.GDApplication;
 import in.fastr.apps.traffic.activities.MainActivity;
+
+import org.acra.ACRA;
+import org.acra.annotation.ReportsCrashes;
+
 import android.content.Intent;
 import android.net.Uri;
 
-import greendroid.app.GDApplication;
-
+@ReportsCrashes(formKey = "dHU2OFRVNXZrV1RBMl9neE1NSGZ6LUE6MQ")
 public class TrafficApplication extends GDApplication {
 
+    @Override
+    public void onCreate() {
+        ACRA.init(this);
+        super.onCreate();
+    }
+    
     @Override
     public Class<?> getHomeActivityClass() {
         return MainActivity.class;
