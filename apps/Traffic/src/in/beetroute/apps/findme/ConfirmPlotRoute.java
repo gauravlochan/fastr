918287798de1fb0f7f6cf18cb88f83a2efd00317
Pradeep Banavara <pradeep@fastr.in>
2012-03-09 16:46:57 +0530

    Following changes have been done: Removed the textview in confirmplotroute to showdialog Added new strings in strings.xml Changed PlotRouteActivity to include the action bar items - Uniform with the main screen

--- apps/Traffic/src/in/beetroute/apps/findme/ConfirmPlotRoute.java
+++ apps/Traffic/src/in/beetroute/apps/findme/ConfirmPlotRoute.java
@@ -2,7 +2,12 @@ package in.beetroute.apps.findme;
 
 
 import in.beetroute.apps.traffic.R;
+import in.beetroute.apps.traffic.activities.MainActivity;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -10,59 +15,48 @@ import android.view.View;
 public class ConfirmPlotRoute extends Activity {
 
     private String toAddress;
+    private static final int DIALOG_ALERT=10;
 	
     @Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.showdialog);
 		Bundle extras = getIntent().getExtras();
-		toAddress = extras.getString("latlon");
+		toAddress = extras.getString("latlon");	
+		showDialog(DIALOG_ALERT);
 	}
 	
-	public void getRouteMap(View view) {
-		if(view.getId() == R.id.button1) {
-			Bundle extras = getIntent().getExtras();
-			toAddress = extras.getString("latlon");
-			Intent intent = new Intent(this, PlotRouteActivity.class);
-			intent.putExtra("latlon", toAddress);
-			startActivity(intent);
-		}
-	}
-	
-	public void doNothing(View view) {
-		if(view.getId() == R.id.button2) {
-			this.finish();
-		}
-	}
-}
-	
-/*
 	protected Dialog onCreateDialog(int id) {
 		
-		String formatString = "Do you want directions for your friend";
+		String formatString = "Your friend has sent their location information. Do you want to plot directions";
 		switch (id) {
 		case DIALOG_ALERT:
 			Builder builder = new AlertDialog.Builder(this);
+			builder.setTitle(R.string.app_name);
 			builder.setMessage(formatString);
 			builder.setCancelable(true);
 			builder.setPositiveButton("OK", new OkOnClickListener());
 			builder.setNegativeButton("Cancel", new CancelOnClickListener());
+			AlertDialog dialog = builder.create();
+			dialog.show();
 		}
 		return super.onCreateDialog(id);
 	}
 	
 	private final class OkOnClickListener implements DialogInterface.OnClickListener {
 		public void onClick(DialogInterface dialog, int which) {
-			Intent intent = new Intent(getApplicationContext(), PlotRouteMap.class);
+			Bundle extras = getIntent().getExtras();
+			toAddress = extras.getString("latlon");
+			Intent intent = new Intent(ConfirmPlotRoute.this, PlotRouteActivity.class);
+			intent.putExtra("latlon", toAddress);
 			startActivity(intent);
 		}
 	}
 
 	private final class CancelOnClickListener implements DialogInterface.OnClickListener {
 		public void onClick(DialogInterface dialog, int which) {
-			ShowDialog.this.finish();
+			ConfirmPlotRoute.this.finish();
+			startActivity(new Intent(ConfirmPlotRoute.this, MainActivity.class));
 		}
 	}	
 }
-
-*/
--- apps/Traffic/src/in/beetroute/apps/findme/PlotRouteActivity.java
+++ apps/Traffic/src/in/beetroute/apps/findme/PlotRouteActivity.java
@@ -1,13 +1,20 @@
 package in.beetroute.apps.findme;
 
 import greendroid.app.GDMapActivity;
+import greendroid.widget.ActionBarItem;
+import greendroid.widget.ActionBarItem.Type;
 import in.beetroute.apps.commonlib.Global;
 import in.beetroute.apps.commonlib.Logger;
 import in.beetroute.apps.traffic.R;
+import in.beetroute.apps.traffic.activities.EnterAddressActivity;
+import in.beetroute.apps.traffic.activities.MainActivity;
+import in.beetroute.apps.traffic.activities.RouteListActivity;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class PlotRouteActivity extends GDMapActivity {
     private static final String TAG = Global.COMPANY;
+    private static final int ENTER_DESTINATION_REQUEST_CODE = 100;
 
     @Override
     protected boolean isRouteDisplayed() {
@@ -20,6 +27,60 @@ public class PlotRouteActivity extends GDMapActivity {
 
         super.onCreate(savedInstanceState);
         setActionBarContentView(R.layout.showroute);
+        
+        //Add a help screen
+        addActionBarItem(Type.Help, R.id.action_bar_help);
+
+        // Add the direction button
+        addActionBarItem(Type.Export, R.id.action_bar_directions);
+        
+        // Add the route history button
+        addActionBarItem(Type.List, R.id.action_bar_routelist);
+        
+        // Add the find me icon to the action bar
+        addActionBarItem(Type.LocateMyself, R.id.action_bar_findme);  
     }
+
+	@Override
+	public boolean onHandleActionBarItemClick(ActionBarItem item, int position) {
+		// TODO Auto-generated method stub
+		 switch (item.getItemId()) {
+         case R.id.action_bar_directions:
+             // Testing: Log the DB here
+             // LocationDbHelper dbHelper = new LocationDbHelper(this, null);
+             // dbHelper.logDatabase();
+
+             startActivityForResult(new Intent(this, EnterAddressActivity.class), 
+             		ENTER_DESTINATION_REQUEST_CODE);
+             break;
+             
+         case R.id.action_bar_routelist:
+             startActivity(new Intent(this, RouteListActivity.class));
+             break;
+         
+         case R.id.action_bar_findme:
+         	//quickAction = new QuickActionGrid(this);
+         	//quickAction.addQuickAction(new QuickAction(getApplicationContext(),R.drawable.gd_action_bar_locate_myself, new String("facebook")));
+         	//quickAction.show(mapView);
+         	startActivity(new Intent(this,SendSMS.class));
+         	break;
+         	
+         case R.id.action_bar_help:
+         	
+         	break;
+         	
+         default:
+             return super.onHandleActionBarItemClick(item, position);
+		 }
+		 return true;
+		
+	}
+
+	@Override
+	public void onBackPressed() {
+		// TODO Auto-generated method stub
+		super.onBackPressed();
+		startActivity( new Intent(this,MainActivity.class));
+	}
 
 }
