8b8c7c708b5038d8e699cfab4731c1939b44de1e
Gaurav Lochan <gaurav@fastr.in>
2012-03-13 02:14:13 +0530

    Remove ACRA.  It's a pain to see debug reports there.

--- apps/Traffic/src/in/beetroute/apps/traffic/TrafficApplication.java
+++ apps/Traffic/src/in/beetroute/apps/traffic/TrafficApplication.java
@@ -4,12 +4,11 @@ import greendroid.app.GDApplication;
 import in.beetroute.apps.traffic.activities.MainActivity;
 
 import org.acra.ACRA;
-import org.acra.annotation.ReportsCrashes;
 
 import android.content.Intent;
 import android.net.Uri;
 
-@ReportsCrashes(formKey = "dHU2OFRVNXZrV1RBMl9neE1NSGZ6LUE6MQ")
+//@ReportsCrashes(formKey = "dHU2OFRVNXZrV1RBMl9neE1NSGZ6LUE6MQ")
 public class TrafficApplication extends GDApplication {
 
     @Override
